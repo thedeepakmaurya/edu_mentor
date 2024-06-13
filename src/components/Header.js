d0b3c05870d69c59c28079580c35f48bbbff6479
thedeepakmaurya <mauryadeepak135@gmail.com>
2024-06-13 22:40:34 +0530

    layout fixed

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className='flex h-14 bg-white items-center pl-5 '>
+    <div className='flex h-[8%] bg-white items-center pl-5 '>
       <h1 className='flex text-black font-bold text-3xl'>Dashboard</h1>
     </div>
   )
--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -4,17 +4,17 @@ import logo from '../assets/img/logo.png'
 const SideMenu = () => {
 
     return (
-        <div className='flex flex-col bg-black items-center text-white h-screen w-52'>
+        <div className='flex flex-col bg-black items-center text-white h-screen w-[14%]'>
             <div className='w-full flex items-center justify-center'>
                 <img className='w-44 mt-5 mb-5' alt='logo' src={logo} />
             </div>
             <div className='flex flex-col gap-2 w-full pl-6'>
-                <h2 className='font-semibold text-lg'><i className='bx bxs-user pr-3'></i> <a href='/login'>Login</a></h2>
-                <h2 className='font-semibold text-lg'><i className='bx bxs-user-plus pr-3'></i> <a href='/register'>Register</a></h2>
-                <h2 className='font-semibold text-lg'><i className='bx bxs-info-circle pr-3'></i> <a href='/about'>About</a></h2>
+                <h2 className='font-semibold text-lg '><i className='bx bxs-user pr-3 align-middle'></i> <a href='/login'>Login</a></h2>
+                <h2 className='font-semibold text-lg'><i className='bx bxs-user-plus pr-3 align-middle'></i> <a href='/register'>Register</a></h2>
+                <h2 className='font-semibold text-lg'><i className='bx bxs-info-circle pr-3 align-middle'></i> <a href='/about'>About</a></h2>
             </div>
-            <div className='w-52 pl-6  bottom-5 fixed'>
-                <h2 className='font-semibold text-lg'><i className='bx bx-log-out pr-3'></i> Logout</h2>
+            <div className='w-[14%] pl-6  bottom-5 fixed'>
+                <h2 className='font-semibold text-lg'><i className='bx bx-log-out pr-3 align-middle'></i> Logout</h2>
             </div>
         </div>
     )
