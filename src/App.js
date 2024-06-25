a0937eb414e48819961a0866d15b8327576a24fd
thedeepakmaurya <mauryadeepak135@gmail.com>
2024-06-25 21:05:28 +0530

    mobile view update

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import SideMenu from "./components/SideMenu";
 import { FirebaseProvider } from "./utils/Firebase";
+import notify from '../src/assets/img/notify.svg'
 
 const App = () => {
   return (
     <FirebaseProvider>
-    <div className="sm:visible sm:flex hidden items-center h-screen justify-center">
+    <div className="sm:visible sm:flex sm:flex-col gap-5 hidden items-center h-screen justify-center">
+      <img src={notify} alt="notify" className="w-44" />
        <h1 className="font-bold text-2xl text-oxfordBlue underline">Accessible on desktop!</h1>
     </div>
       <div className="sm:hidden md:hidden bg-platinum h-screen flex overflow-scroll">
