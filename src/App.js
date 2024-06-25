775cadab39c9604998e4e1ab07d2f624f6ab38d3
thedeepakmaurya <mauryadeepak135@gmail.com>
2024-06-25 20:56:34 +0530

    hide on mobile view

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ import { FirebaseProvider } from "./utils/Firebase";
 const App = () => {
   return (
     <FirebaseProvider>
-      <div className="bg-platinum h-screen flex overflow-scroll">
+    <div className="sm:visible sm:flex hidden items-center h-screen justify-center">
+       <h1 className="font-bold text-2xl text-oxfordBlue underline">Accessible on desktop!</h1>
+    </div>
+      <div className="sm:hidden md:hidden bg-platinum h-screen flex overflow-scroll">
         <SideMenu />
         <div className="w-[85%] overflow-scroll">
           <Outlet />
